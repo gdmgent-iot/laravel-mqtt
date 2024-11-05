f5bde107e570db61c0b8324aad9775cdc43b0718
Frederick Roegiers <frederick@rogerthat.be>
2024-11-05 10:32:24 +0100

    cool sample application for digexp2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AnimalController;
+
+// api routes
+// GROUP with api prefix
+Route::prefix('api')->group(function () {
+    Route::get('/beestje', [AnimalController::class, 'animalByHeight'])->name('animalByHeight');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('interface');
 });
